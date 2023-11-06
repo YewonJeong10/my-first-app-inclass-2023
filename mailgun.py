8a0e592ee3d456e0813c98e55f5c10d76806ab6f
YewonJeong1 <yj242@georgetown.edu>
2023-11-06 17:30:22 -0500

    update

---
+++ mailgun.py
@@ -0,0 +1,43 @@
+# example ".env file" contents:
+MAILGUN_API_KEY="__________"
+MAILGUN_SENDER_ADDRESS="example@myschool.edu"
+MAILGUN_DOMAIN="sandbox__________.mailgun.org"
+
+
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
+MAILGUN_SENDER_ADDRESS = os.getenv("MAILGUN_SENDER_ADDRESS")
+MAILGUN_DOMAIN = os.getenv("MAILGUN_DOMAIN") # "sandbox__________.mailgun.org"
+
+def send_email(recipient_address=MAILGUN_SENDER_ADDRESS, subject="[Shopping Cart App] Testing 123", html_content="<p>Hello World</p>"):
+    print("SENDING EMAIL TO:", recipient_address)
+    print("SUBJECT:", subject)
+    print("HTML:", html_content)
+
+    try:
+        request_url = f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages"
+        message_data = {
+            'from': MAILGUN_SENDER_ADDRESS,
+            'to': recipient_address,
+            'subject': subject,
+            'html': html_content,
+        }
+        response = requests.post(request_url,
+            auth=('api', MAILGUN_API_KEY),
+            data=message_data
+        )
+        print("RESULT:", response.status_code)
+        response.raise_for_status()
+        print("Email sent successfully!")
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending email: {str(e)}")
+
+
+
+send_email()
