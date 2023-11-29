8f803adcc5c6d8169417fb663043ff76d317be68
YewonJeong1 <yj242@georgetown.edu>
2023-11-29 18:01:02 -0500

    Configure server

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.unemployment_routes import unemployment_routes
+from web_app.routes.stocks_routes import stocks_routes
 #from web_app.routes.book_routes import book_routes
 #from web_app.routes.weather_routes import weather_routes
 
@@ -16,6 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(home_routes)
     app.register_blueprint(unemployment_routes)
+    app.register_blueprint(stocks_routes)
     app.config["SECRET_KEY"] = SECRET_KEY
     #app.register_blueprint(book_routes)
     #app.register_blueprint(weather_routes)
